3dd71f27a1789679c9084a55841a249eaf51ea11
Мдфв <v.v.titerez@gmail.com>
2026-04-04 22:08:32 +0300

    some fixes

--- EntityFramework/Entity Framework Core/Models/Book.cs
+++ EntityFramework/Entity Framework Core/Models/Book.cs
@@ -4,7 +4,7 @@ public class Book
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public DateTime Year { get; set; }
+    public DateOnly Year { get; set; }
     public int AuthorId { get; set; }
     public Author Author { get; set; }
 }
--- EntityFramework/Entity Framework Core/Program.cs
+++ EntityFramework/Entity Framework Core/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Entity_Framework_Core.Data;
+using Entity_Framework_Core.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entity_Framework_Core;
@@ -8,7 +9,19 @@ public class Program
     public static void Main()
     {
         using var context = new LibraryContext();
-        // context.Database.EnsureCreated();
+        context.Database.EnsureCreated();
+
+        context.Authors.AddRange(
+                new Author {Country = "Canada", Name = "Peater"},
+                new Author {Country = "Moldova", Name = "Belii"}
+            );
+        context.Books.AddRange(
+                new Book{ Title = "How I RTSed", AuthorId = 3, Year = new DateOnly(2025, 10, 12)},
+                new Book{ Title = "How I Forest", AuthorId = 3, Year = new DateOnly(2025, 10, 12)},
+                new Book{ Title = "Canada is beautiful", AuthorId = 2, Year = new DateOnly(2025, 10, 12)},
+                new Book{ Title = "I hate USA", AuthorId = 2, Year = new DateOnly(2024, 10, 14)}
+            );
+        context.SaveChanges();
 
         var authors = context.Authors.Include(a => a.Books).ToList();
         foreach (var author in authors)
